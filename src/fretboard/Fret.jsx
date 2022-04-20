d32ba04698811bbeeb2c0a070478fc376e93f8b3
Aby <abybenrajan@gmail.com>
2022-04-20 16:09:10 +0530

    added layers

--- src/fretboard/Fret.jsx
+++ src/fretboard/Fret.jsx
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Fret = ({ offset, stringName }) => {
+    const [selected, setSelected] = useState(true);
+
+    const handleClick = () => {
+        setSelected(!selected);
+    };
+
+    const renderLayer = () => {
+        if (selected) {
+            return (
+                <div className="layer-0">
+                    {offset === 0 ? stringName : offset}
+                </div>
+            );
+        } else {
+            return (
+                <div className="layer-1">
+                    {offset === 0 ? stringName : offset}
+                </div>
+            );
+        }
+    }
+
     return (
-        <div className={`fret ${offset === 0 ? 'fretzero' : ''}`}>
-            <div className="layer-0">
-                {offset === 0 ? stringName : offset}
-            </div>
+        <div
+            onClick={handleClick}
+            className={`fret ${offset === 0 ? 'fretzero' : ''}`}
+        >
+            {renderLayer()}
         </div>
     );
 };
